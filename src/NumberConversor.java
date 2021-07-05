398303ccf1ef52269a37fa7dc2c7441ca0745be1
Damian Arp <damianarp@gmail.com>
2021-07-05 15:48:50 -0300

    Add try-catch

--- src/NumberConversor.java
+++ src/NumberConversor.java
@@ -2,15 +2,28 @@
 import javax.swing.*;
 
 public class NumberConversor {
+    public static int decimalNumber;
+    public static String message;
+
+    // Main method
     public static void main(String[] args) {
+
+        // Show a window where to insert number (which by default, it will be a String) and parse it to Integer to be used.
+        String numberStr = JOptionPane.showInputDialog(null, "Insert a number" );
+
+        // Use try-catch to manage the exception.
+        try {
+            decimalNumber = Integer.parseInt(numberStr); // In case the input be a number, the app to be continued.
+        }catch(NumberFormatException e){
+            // In case the input not be a number, show a message and restart a main method.
+            JOptionPane.showMessageDialog(null, "Error! Please, insert a number" );
+            main(args);
+            System.exit(0);
+        }
+
         convertNumber();
         showMessage();
     }
-
-    // Show a window where to insert number (which by default, it will be a String) and parse it to Integer to be used.
-    public static String numberStr = JOptionPane.showInputDialog(null, "Insert a number" );
-    public static int decimalNumber = Integer.parseInt(numberStr);
-    public static String message;
 
     // Message's body and conversions.
     public static void convertNumber(){
